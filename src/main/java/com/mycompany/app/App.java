9cad1b5844eb0e40c082aa52c63aaa2a0fde13d1
Ulf Larson <ulf.e.larson@gmail.com>
2021-12-06 12:41:34 -0800

    changed print

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Crazy Crazy World!" );
+        System.out.println( "Hello Unbelievably x Crazy World!" );
     }
 }
